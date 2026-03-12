e1201dba951cf653c3fb2329c68f146dea88683d
LdMe <dlafuente003@gmail.com>
2026-03-12 10:55:55 +0100

    storage manager y renderizar favoritos

--- src/App.js
+++ src/App.js
@@ -3,51 +3,81 @@ import { GenericCollection, GameCollection } from "./core/Collections.js";
 import Game from "./core/Game.js";
 import Genre from "./core/Genre.js";
 import DOMManager from "./managers/DOMManager.js";
+import StorageManager from "./managers/StorageManager.js";
 class App {
     constructor() {
         this.games = null;
         this.genres = null;
         this.domManager = new DOMManager();
+        this.showFavorites = false;
+        this.gamesSection = document.getElementById("games-list");
+        this.genresSection = document.getElementById("genres-list");
     }
     async init() {
+        // genres
+        const rawGenres = await APIManager.getRawGenres();
+        this.genres = this.createGenres(rawGenres);
+
+        if (this.genresSection) {
+            this.domManager.renderGenres(this.genres.getElements(), this.genresSection)
+        }
+
+        // games
         const rawGames = await APIManager.getRawGames();
         this.games = this.createGames(rawGames);
 
-        const gamesSection  = document.getElementById("games-list");
-        if(gamesSection) {
-            this.domManager.renderGames(this.games.getElements(),gamesSection)
-        }
+        this.loadFavorites();
 
-        const rawGenres = await APIManager.getRawGenres();
-        this.genres = this.createGenres(rawGenres);
-        const genresSection =  document.getElementById("genres-list");
-        if(genresSection){
-            this.domManager.renderGenres(this.genres.getElements(),genresSection)
-        }
+        this.renderGames();
 
-        this.setupListeners();
+        
 
+        
 
     }
 
-    setupListeners(){
-        const buttons = [...document.getElementsByClassName("fav-button")];
-        buttons.forEach(button =>{
-            button.addEventListener("click",()=>{
-                const id = parseInt(button.dataset.gameId);
-                this.handleToggleFavorite(id);
-                const isGameFavorite = this.games.isFavorite(id);
-                if(isGameFavorite){
-                    button.textContent = "Quitar de favoritos";
-                }else{
-                    button.textContent ="Añadir a favoritos";
-                }
-
-            })
+    renderGames() {
+        if (this.gamesSection) {
+            const games = this.showFavorites ? this.games.getFavorites() : this.games.getElements();
+            this.domManager.renderGames(games, this.gamesSection)
+        }
+        this.setupListeners();
+    }
+    loadFavorites() {
+        const favoriteIds = StorageManager.getFavorites();
+        favoriteIds.forEach(favId => {
+            this.games.addToFavorites(favId);
         })
     }
-    handleToggleFavorite(id){
+    setupListeners() {
+        // listeners favoritos
+        this.domManager.setupListeners(".fav-button", "click", (e) => {
+            const button = e.currentTarget;
+            const id = parseInt(button.dataset.gameId);
+            this.handleToggleFavorite(id);
+            const isGameFavorite = this.games.isFavorite(id);
+            if (isGameFavorite) {
+                button.textContent = "Quitar de favoritos";
+            } else {
+                button.textContent = "Añadir a favoritos";
+            }
+        })
+
+        //listeners navbar
+        this.domManager.setupListeners("#navbar li", "click", (e) => {
+            const element = e.currentTarget;
+            const route = element.dataset.route;
+            if (route === "home") {
+                this.showFavorites = false;
+            } else if (route === "fav") {
+                this.showFavorites = true;
+            }
+            this.renderGames();
+        })
+    }
+    handleToggleFavorite(id) {
         this.games.toggleFavorite(id);
+        StorageManager.toggleFavorite(id);
     }
     createGames(gamesData) {
         // return gamesData.map(game => new Game(game.id,game.name,game.background_image,game.rating));
--- src/managers/DOMManager.js
+++ src/managers/DOMManager.js
@@ -2,20 +2,21 @@ class DOMManager {
     constructor() {
 
     }
-    renderGames(games,parent){
-        const htmlGames = games.map(game =>this.createGameHtml(game));
+    renderGames(games, parent) {
+        const htmlGames = games.map(game => this.createGameHtml(game));
+        parent.innerHTML = "";
         parent.append(...htmlGames);
     }
-    createGameHtml(game){
+    createGameHtml(game) {
         // contentedor (tarjeta)
         const article = document.createElement("article");
         article.classList.add("game-card")
-        article.setAttribute("id",`game-${game.id}`);
+        article.setAttribute("id", `game-${game.id}`);
 
         // imagen
         const image = document.createElement("img");
-        image.setAttribute("alt",`portada del juego ${game.name}`);
-        image.setAttribute("src",game.backgroundImage);
+        image.setAttribute("alt", `portada del juego ${game.name}`);
+        image.setAttribute("src", game.backgroundImage);
 
         // titulo
         const title = document.createElement("h3");
@@ -24,36 +25,49 @@ class DOMManager {
         // rating
         const rating = document.createElement("p");
         rating.classList.add("rating");
-        rating.textContent = "rating: "+game.rating;
+        rating.textContent = "rating: " + game.rating;
 
         // boton favoritos
         const favButton = document.createElement("button");
-        favButton.setAttribute("id",`fav-${game.id}`);
-        favButton.dataset.gameId= game.id;
+        favButton.setAttribute("id", `fav-${game.id}`);
+        favButton.dataset.gameId = game.id;
         favButton.classList.add("fav-button");
-        favButton.textContent = "Añadir a favoritos";
+        if (game.isFavorite) {
+            favButton.textContent = "Quitar de favoritos";
+        } else {
+            favButton.textContent = "Añadir a favoritos";
+
+        }
 
 
 
-        article.append(image,title,rating,favButton);
+        article.append(image, title, rating, favButton);
         return article;
 
     }
-    renderGenres(genres,parent){
-        const htmlGenres = genres.map(genre =>this.createGenreHtml(genre));
+    renderGenres(genres, parent) {
+        const htmlGenres = genres.map(genre => this.createGenreHtml(genre));
+        parent.innerHTML = "";
         parent.append(...htmlGenres);
     }
-    createGenreHtml(genre){
+    createGenreHtml(genre) {
         const article = document.createElement("article");
         article.classList.add("genre-card")
-        article.setAttribute("id",`genre-${genre.id}`);
+        article.setAttribute("id", `genre-${genre.id}`);
         article.style.backgroundImage = genre.backgroundImage;
 
         const title = document.createElement("h3");
         title.innerText = genre.name;
-        
+
         article.appendChild(title);
         return article;
+
+    }
+    setupListeners(query,eventType,callback){
+        const elements = [...document.querySelectorAll(query)];
+        elements.forEach(element=>{
+            element.addEventListener(eventType,callback)
+        })
 
     }
 }
